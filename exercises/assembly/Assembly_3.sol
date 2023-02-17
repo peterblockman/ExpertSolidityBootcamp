7b18121f46196542ebfa66b3bcc66a3be2017e0d
peterblockman <peterblockman.git@protonmail.com>
2023-02-17 09:39:01 +0700

    Assembly 3

--- exercises/assembly/Assembly_3.sol
+++ exercises/assembly/Assembly_3.sol
@@ -5,8 +5,21 @@ contract SubOverflow {
     // otherwise it should return x - y
     function subtract(uint256 x, uint256 y) public pure returns (uint256) {
         // Write assembly code that handles overflows
-        assembly {
+        uint256 diff;
+        bool success;
 
+        assembly {
+            diff := sub(x, y)
+            // overflow when x < y or diff > x
+            let isOverflow := or(lt(x, y),gt(diff, x))
+            if iszero(isOverflow) {
+               success := true
+            }
         }
+
+        if(success){
+            return diff;
+        } 
+        return 0;
     }
 }
